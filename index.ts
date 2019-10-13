81bdcee71d416f2f4a60ef2b1fd5f83f3a078466
Javier Reta <reivaj79mx@gmail.com>
2019-10-13 00:03:58 -0500

    Respaldo del servidor de Sockets

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import router from "./routes/router";
 import bodyparser from "body-parser";
 import cors from 'cors';
 
-const server = new Server;
+const server = Server.instance;
 
 // body parser
 server.app.use(bodyparser.urlencoded({ extended: true }));
